0a643ae76724c2a4641f84dcec4f80a14fa130a4
TZ <Tottoszoltan01@gmail.com>
2026-09-20 22:02:36 +0200

    bump PWA cache to 0.0.4

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION="0.0.3";
+const VERSION="0.0.4";
 const CACHE="megalife-"+VERSION;
 const CORE=["./","./index.html","./styles.css","./app.js","./manifest.json","./icon.svg"];
 self.addEventListener("install",event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()))});
