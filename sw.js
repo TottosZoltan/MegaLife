1ea92c3648044e46df5e870da47bca0dd407203e
TZ <Tottoszoltan01@gmail.com>
2026-09-20 22:16:31 +0200

    refresh PWA cache for 0.0.9

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION="0.0.8";
+const VERSION="0.0.9";
 const CACHE="megalife-"+VERSION;
 const CORE=["./","./index.html","./styles.css","./app.js","./manifest.json","./icon.svg"];
 self.addEventListener("install",event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()))});
