2ada25d8c006b91b3244a0da301f0fa83ae2ed85
TZ <Tottoszoltan01@gmail.com>
2026-09-21 13:41:27 +0200

    v0.0.31 version bump

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION="0.0.30";
+const VERSION="0.0.31";
 const CACHE="megalife-"+VERSION;
 const CORE=["./","./index.html","./styles.css","./app.js","./manifest.json","./icon.svg"];
 self.addEventListener("install",event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()))});
